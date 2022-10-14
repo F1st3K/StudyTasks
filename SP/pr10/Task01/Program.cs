2870b2ac4ccbcb2780c8241a63b4b2cec6095a90
F1st3K <kostinnikita988@gmail.com>
2022-10-15 00:14:32 +0300

    SP->pr10

--- SP/pr10/Task01/Program.cs
+++ SP/pr10/Task01/Program.cs
@@ -11,12 +11,12 @@ namespace Task01
         /*Задание 09. Вычисление площади треугольника, если известна длина основания и высота
         Введите исходные данные:
         Основание (см) - xx.xx
-        Высота (см) - xx
+        Высота (см) - xxd
         Площадь треугольника: xx.xx кв.см.*/
         static void Main(string[] args)
         {
             double Found, Height, Square;
-            Console.WriteLine("Found: ");
+            Console.WriteLine("Foundd: ");
             Found = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Height: ");
             Height = Convert.ToDouble(Console.ReadLine());
